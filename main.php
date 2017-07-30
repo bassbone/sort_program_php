fba0a1ce4b5f53e391ec07b93f571f0ccd95b492
bassbone <bassbone0o_o0@yahoo.co.jp>
2017-07-31 00:39:16 +0900

    挿入ソート

--- main.php
+++ main.php
@@ -43,7 +43,25 @@ class SelectionSort implements Algorithm {
     }
 }
 
-$sort = new Sort(new SelectionSort());
+class InsertionSort implements Algorithm {
+    public function sort(array $list) : array {
+        $length = count($list);
+        for ($i = 1; $i < $length; $i++) {
+            $tmp = $list[$i];
+            if ($list[$i - 1] > $tmp) {
+                $j = $i;
+                do {
+                    $list[$j] = $list[$j - 1];
+                    $j--;
+                } while ($j > 0 && $list[$j - 1] > $tmp);
+                $list[$j] = $tmp;
+            }
+        }
+        return $list;
+    }
+}
+
+$sort = new Sort(new InsertionSort());
 
 exit;
 
@@ -60,6 +78,7 @@ class Sort{
         $time_start = microtime(true);
 
         $this->list = $sort->sort($this->list);
+        //print(implode(',', $this->list).PHP_EOL);
 
         $time = microtime(true) - $time_start;
         echo "{$time} 秒".PHP_EOL;
